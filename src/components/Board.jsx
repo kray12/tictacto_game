05dce4e0c5fbc63085e9a879c9baf41561dc4e2a
kray12 <karandavande12@gmail.com>
2023-03-02 21:25:59 +0530

    add players and play functionality

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 import Sq from './Sq.jsx'
 const Board = () => {
-    const [sqrs,setSqrs]=useState(Array(9).fill(null));
-    console.log(sqrs)
-    const sqClick = (clickedSq)=>{
-        setSqrs(currentSq=>{
+    const [player,setNextPlayer] = useState(true); // set players turn.
+    const [sqrs,setSqrs]=useState(Array(9).fill(null)); // Asume board as an array with 9 sqs.
+    console.log(sqrs); // to print array of sqs.
+    const sqClick = (clickedSq)=>{ // Mark sq. functions on board.
+        if(sqrs[clickedSq]){
+            return; // exit condition to prevent rewrite sq.
+        } 
+        setSqrs(currentSq=>{ // update sqs.
             return currentSq.map((mappedArr,spot)=>{
                 if(clickedSq === spot){
-                    return "X";
+                    return player ? "X" : "O"; // set players input.
                 }
                 // return console.log(sqrs);
                 return mappedArr;
@@ -15,14 +19,15 @@ const Board = () => {
         })
     
         // console.log('clicked')
+        setNextPlayer((player => !player)) // reset players
     }
+// main container  --
     const renderSq = spot => {
         return (
             <Sq value={sqrs[spot]} 
                 onClick={()=>sqClick(spot)}
             />
         )
-
     }
     return (
     <div className="board">
